3df82ba4adacebf92640a2a3fbc14d96b093faf8
haileyham <109202954+haileyham@users.noreply.github.com>
2023-09-03 01:05:43 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,10 +1,4 @@
 function solution(n) {
-    let 수박수박 = []
-    let 수박제조기 = 'n'.repeat(n).split('').map((a,i)=>
-                           {if(a[i%2]){
-        수박수박.push("수")
-    }else{
-        수박수박.push("박")
-    }})
-    return 수박수박.join('')
+    let 수박제조기 = "수박".repeat(n)
+    return 수박제조기.substring(0,n);
 }
