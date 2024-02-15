11528824d4428de65ca12b190dae9ee34b592321
haileyham <109202954+haileyham@users.noreply.github.com>
2024-02-15 13:10:01 +0900

    [Silver III] Title: 소수 구하기, Time: 308 ms, Memory: 31684 KB -BaekjoonHub

--- 백준/Silver III/1929. 소수 구하기/소수 구하기.js
+++ 백준/Silver III/1929. 소수 구하기/소수 구하기.js
@@ -1,27 +1,21 @@
 const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "../input.txt").toString().trim().split(" ").map(Number);
 
-function prime(input) {
-  const primes = [];
+function prime(n) {
+  const maxNum = n[1];
+  const arr = Array.from({ length: maxNum + 1 }).fill(true);
+  arr[0] = arr[1] = false;
+  const sqrt = parseInt(Math.sqrt(maxNum));
 
-  for (let num = input[0]; num <= input[1]; num++) {
-    let isPrime = true;
-
-    if (num <= 1) {
-      isPrime = false;
-    } else {
-      for (let i = 2; i * i <= num; i++) {
-        if (num % i === 0) {
-          isPrime = false;
-          break;
-        }
+  for (let i = 2; i <= sqrt; i++) {
+    if (arr[i] === true) {
+      for (let j = 2; i * j <= maxNum; j++) {
+        arr[i * j] = false;
       }
     }
-    if (isPrime) {
-      primes.push(num);
-    }
   }
-  return primes;
+
+  return arr.map((v, i) => v ? i : -1).filter((i) => i !== -1 && i >= n[0]);
 }
 
 const answer = prime(input).join("\n");
-console.log(answer); 
+console.log(answer);
